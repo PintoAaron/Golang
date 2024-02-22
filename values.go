feb0187e7966d4f4c42242f8767315a5c87e08f0
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-02-22 09:19:35 +0000

    working with values and variables

---
+++ values.go
@@ -0,0 +1,19 @@
+package main
+
+
+import "fmt"
+
+
+func main(){
+	//trying something with values and variables
+	var is_active = true
+	fmt.Println("Master" + "Chief")
+	fmt.Println("5+5 = ",5+5)
+	fmt.Println("9.0/3.0 = ",9.0/3.0)
+	fmt.Println(!is_active)
+	fmt.Println("10%3 = ",10%3)
+	fmt.Println("2*3 = ", 2 != 3)
+
+	count := 50
+	fmt.Println(count)
+}
