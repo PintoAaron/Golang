90e87ceb85cc9790c0998cedfe8c1939ae7d4175
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-03-13 14:32:45 +0000

    variadic function

---
+++ variadic_function.go
@@ -0,0 +1,42 @@
+package main 
+
+
+import "fmt"
+
+
+func sum_all(nums...int) int{
+	total := 0
+	for _,num := range nums{
+
+		total += num
+	}
+
+	return total
+}
+
+
+
+func longest_word(words...string) (string,int){
+	temp := ""
+	for _,word := range words{
+		if len(word) > len(temp){
+			temp = word
+		}
+	}
+
+	return temp,len(temp)
+}
+
+
+func main(){
+
+	res := sum_all(4,5,2,1,3,3,4,21,2,5,3,2,1,3,8,9,)
+	fmt.Printf("Total is %d\n",res)
+
+
+	word,word_length := longest_word("Master","encyclopedia","opotptot","givatheboyback","macquena","seriousuy")
+	fmt.Printf("The longest word is %s with %d characters.",word,word_length)
+
+
+	
+}
